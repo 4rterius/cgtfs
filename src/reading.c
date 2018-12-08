d7ee858a00834f63cd9cc080c74c206b0baff7bd
aMomentaryLapseOfFantasy <finalso23@gmail.com>
2018-12-08 18:38:52 +0200

    fare_attributes file reader

--- src/reading.c
+++ src/reading.c
@@ -68,3 +68,24 @@ int read_all_calendar_records(FILE *fp, calendar_record_t **records) {
 
     return record_count;
 }
+
+int read_all_fare_attributes(FILE *fp, fare_attributes_t **records) {
+    char **field_names;
+    int field_count = read_header(fp, &field_names);
+
+    char **record_values;
+    int record_count = 0;
+    
+    *records = malloc(sizeof(fare_attributes_t));
+    
+    while (read_record(fp, field_count, &record_values) > 0) {
+        *records = realloc(*records, (record_count + 1) * sizeof(agency_t));
+        (*records)[record_count] = read_fare_attributes(field_count, field_names, record_values);
+        record_count++;
+    }
+
+    free(field_names);
+    free(record_values);
+
+    return record_count;
+}
