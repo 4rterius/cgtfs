83e4838277ca8e9ef0f0a5f226b12148d7a0b84d
Alex Sobolev <finalso23@gmail.com>
2019-05-28 21:04:41 +0300

    Single-file include of all database record tests, tests files renamed

---
+++ tests/tests-database_records.c
@@ -0,0 +1,20 @@
+#ifndef CGTFS_TESTS_DATABASE_RECORDS_C
+#define CGTFS_TESTS_DATABASE_RECORDS_C
+
+#include "greatest/greatest.h"
+
+#include "tests-database_records_agency.c"
+#include "tests-database_records_calendar_dates.c"
+#include "tests-database_records_calendar.c"  // calendar_records
+#include "tests-database_records_fare_attributes.c"
+#include "tests-database_records_fare_rule.c"  // fare_rules
+#include "tests-database_records_feed_info.c"
+#include "tests-database_records_frequency.c"  // frequencies
+#include "tests-database_records_route.c"  // routes
+#include "tests-database_records_shape.c"  // shapes
+#include "tests-database_records_stop_time.c"  // stop_times
+#include "tests-database_records_stop.c"  // stops
+#include "tests-database_records_transfers.c"
+#include "tests-database_records_trip.c"  // trips
+
+#endif
