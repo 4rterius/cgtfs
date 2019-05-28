9791a4d9b06368a08f549bdbe717750cfd303b19
Alex Sobolev <finalso23@gmail.com>
2019-05-28 21:08:47 +0300

    Dedicated folder for db records tests and another for records tests

--- tests/tests-database_records.c
+++ tests/tests-database_records.c
@@ -3,18 +3,18 @@
 
 #include "greatest/greatest.h"
 
-#include "tests-database_records_agency.c"
-#include "tests-database_records_calendar_dates.c"
-#include "tests-database_records_calendar.c"  // calendar_records
-#include "tests-database_records_fare_attributes.c"
-#include "tests-database_records_fare_rule.c"  // fare_rules
-#include "tests-database_records_feed_info.c"
-#include "tests-database_records_frequency.c"  // frequencies
-#include "tests-database_records_route.c"  // routes
-#include "tests-database_records_shape.c"  // shapes
-#include "tests-database_records_stop_time.c"  // stop_times
-#include "tests-database_records_stop.c"  // stops
-#include "tests-database_records_transfers.c"
-#include "tests-database_records_trip.c"  // trips
+#include "tests_database_records/tests-database_records_agency.c"
+#include "tests_database_records/tests-database_records_calendar_dates.c"
+#include "tests_database_records/tests-database_records_calendar.c"  // calendar_records
+#include "tests_database_records/tests-database_records_fare_attributes.c"
+#include "tests_database_records/tests-database_records_fare_rule.c"  // fare_rules
+#include "tests_database_records/tests-database_records_feed_info.c"
+#include "tests_database_records/tests-database_records_frequency.c"  // frequencies
+#include "tests_database_records/tests-database_records_route.c"  // routes
+#include "tests_database_records/tests-database_records_shape.c"  // shapes
+#include "tests_database_records/tests-database_records_stop_time.c"  // stop_times
+#include "tests_database_records/tests-database_records_stop.c"  // stops
+#include "tests_database_records/tests-database_records_transfers.c"
+#include "tests_database_records/tests-database_records_trip.c"  // trips
 
 #endif
