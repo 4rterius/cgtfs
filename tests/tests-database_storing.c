f658024c0f74a115053415ae56cb1dc382fbd7b4
Alex Sobolev <finalso23@gmail.com>
2019-05-29 13:18:27 +0300

    Test gtfs set, table storing test db new name

--- tests/tests-database_storing.c
+++ tests/tests-database_storing.c
@@ -6,15 +6,15 @@
 
 
 TEST db_all_agencies_store(void) {
-    FILE *fp = fopen("../tests/data/agency.txt", "r");
+    FILE *fp = fopen("../tests/data/stupid_gtfs/agency.txt", "r");
     if (fp == NULL) {
-        FAILm("Couldn't open `data/agency.txt` test file");
+        FAILm("Couldn't open `data/stupid_gtfs/agency.txt` test file");
     } else {
         
         feed_db_t db;
         // feed_db_status_t res;
 
-        /* res = */ init_feed_db(&db, "tests_store_agencies0.db", 1);
+        /* res = */ init_feed_db(&db, "tests_storing.db", 1);
         /* res = */ setup_feed_db(&db, 1);
 
         int stored_count = store_all_agencies_db(fp, &db);
@@ -27,19 +27,19 @@ TEST db_all_agencies_store(void) {
 }
 
 TEST db_all_calendar_dates_store(void) {
-    FILE *fp = fopen("../tests/data/pocono_pony/calendar_dates.txt", "r");
+    FILE *fp = fopen("../tests/data/stupid_gtfs/calendar_dates.txt", "r");
     if (fp == NULL) {
-        FAILm("Couldn't open `data/pocono_pony/calendar_dates.txt` test file");
+        FAILm("Couldn't open `data/stupid_gtfs/calendar_dates.txt` test file");
     } else {
         
         feed_db_t db;
         // feed_db_status_t res;
 
-        /* res = */ init_feed_db(&db, "tests_store_agencies0.db", 1);
+        /* res = */ init_feed_db(&db, "tests_storing.db", 1);
         /* res = */ setup_feed_db(&db, 1);
 
         int stored_count = store_all_calendar_dates_db(fp, &db);
-        ASSERT_EQ_FMT(138, stored_count, "%i");
+        ASSERT_EQ_FMT(9, stored_count, "%i");
 
         free_feed_db(&db);
         if (fp) fclose(fp);
