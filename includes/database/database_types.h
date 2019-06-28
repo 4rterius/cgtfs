fca77ea287342230ebad0c9a2b619dae02c17ae5
Alex Sobolev <finalso23@gmail.com>
2019-06-28 17:59:02 +0300

    Db fetching and storing, database types in-line documentation

--- includes/database/database_types.h
+++ includes/database/database_types.h
@@ -8,20 +8,20 @@
  * Base struct of all db processes to encapsulate the DB realization.
  */
 typedef struct feed_db_t {
-    char *path;
-    sqlite3* conn;
-    char *error_msg;
-    int rc;
-    int open;
+    char *path;         ///< /path/to/database.file
+    sqlite3* conn;      ///< Underlying sqlite3 connection.
+    char *error_msg;    ///< Field for storing sqlite error messages. Populate with strdup()
+    int rc;             ///< Field for storing sqlite return codes.
+    int open;           ///< Whether this connection has been opened or not (0 || 1)
 } feed_db_t;
 
 /**
  * Possible outcomes of feed db operations.
  */
 typedef enum feed_db_status_t {
-    FEED_DB_ERROR,
-    FEED_DB_SUCCESS,
-    FEED_DB_PARTIAL
+    FEED_DB_ERROR = 0,      ///< Miserably failed, check error message or return code.
+    FEED_DB_SUCCESS = 1,    ///< Successfully done whatever.
+    FEED_DB_PARTIAL = 2     ///< Kinda succeeded, but not fully. (Might indicate some files missing from a feed.)
 } feed_db_status_t;
 
 #endif
--- includes/database/storing.h
+++ includes/database/storing.h
@@ -11,18 +11,147 @@
 #include "mem_utils.h"
 
 
+/**
+ * Stores a file of agency_t records 
+ * into a given feed database instance.
+ * 
+ * @param[in]     fp    Opened file to source data from.
+ * @param[out]    db    Initialized feed database instance to populate.
+ * 
+ * @returns       Number of stored records.
+ */
 int store_all_agencies_db(FILE *fp, feed_db_t *db);
+
+/**
+ * Stores a file of calendar_date_t records 
+ * into a given feed database instance.
+ * 
+ * @param[in]     fp    Opened file to source data from.
+ * @param[out]    db    Initialized feed database instance to populate.
+ * 
+ * @returns       Number of stored records.
+ */
 int store_all_calendar_dates_db(FILE *fp, feed_db_t *db);
+
+/**
+ * Stores a file of calendar_record_t records 
+ * into a given feed database instance.
+ * 
+ * @param[in]     fp    Opened file to source data from.
+ * @param[out]    db    Initialized feed database instance to populate.
+ * 
+ * @returns       Number of stored records.
+ */
 int store_all_calendar_records_db(FILE *fp, feed_db_t *db);
+
+/**
+ * Stores a file of fare_attributes_t records 
+ * into a given feed database instance.
+ * 
+ * @param[in]     fp    Opened file to source data from.
+ * @param[out]    db    Initialized feed database instance to populate.
+ * 
+ * @returns       Number of stored records.
+ */
 int store_all_fare_attributes_db(FILE *fp, feed_db_t *db);
+
+/**
+ * Stores a file of fare_rule_t records 
+ * into a given feed database instance.
+ * 
+ * @param[in]     fp    Opened file to source data from.
+ * @param[out]    db    Initialized feed database instance to populate.
+ * 
+ * @returns       Number of stored records.
+ */
 int store_all_fare_rules_db(FILE *fp, feed_db_t *db);
+
+/**
+ * Stores a file of feed_info_t records 
+ * into a given feed database instance.
+ * 
+ * @param[in]     fp    Opened file to source data from.
+ * @param[out]    db    Initialized feed database instance to populate.
+ * 
+ * @returns       Number of stored records.
+ */
 int store_all_feed_info_db(FILE *fp, feed_db_t *db);
+
+/**
+ * Stores a file of frequency_t records 
+ * into a given feed database instance.
+ * 
+ * @param[in]     fp    Opened file to source data from.
+ * @param[out]    db    Initialized feed database instance to populate.
+ * 
+ * @returns       Number of stored records.
+ */
 int store_all_frequencies_db(FILE *fp, feed_db_t *db);
+
+/**
+ * Stores a file of route_t records 
+ * into a given feed database instance.
+ * 
+ * @param[in]     fp    Opened file to source data from.
+ * @param[out]    db    Initialized feed database instance to populate.
+ * 
+ * @returns       Number of stored records.
+ */
 int store_all_routes_db(FILE *fp, feed_db_t *db);
+
+/**
+ * Stores a file of shape_t records 
+ * into a given feed database instance.
+ * 
+ * @param[in]     fp    Opened file to source data from.
+ * @param[out]    db    Initialized feed database instance to populate.
+ * 
+ * @returns       Number of stored records.
+ */
 int store_all_shapes_db(FILE *fp, feed_db_t *db);
+
+/**
+ * Stores a file of stop_time_t records 
+ * into a given feed database instance.
+ * 
+ * @param[in]     fp    Opened file to source data from.
+ * @param[out]    db    Initialized feed database instance to populate.
+ * 
+ * @returns       Number of stored records.
+ */
 int store_all_stop_times_db(FILE *fp, feed_db_t *db);
+
+/**
+ * Stores a file of stop_t records 
+ * into a given feed database instance.
+ * 
+ * @param[in]     fp    Opened file to source data from.
+ * @param[out]    db    Initialized feed database instance to populate.
+ * 
+ * @returns       Number of stored records.
+ */
 int store_all_stops_db(FILE *fp, feed_db_t *db);
+
+/**
+ * Stores a file of transfer_t records 
+ * into a given feed database instance.
+ * 
+ * @param[in]     fp    Opened file to source data from.
+ * @param[out]    db    Initialized feed database instance to populate.
+ * 
+ * @returns       Number of stored records.
+ */
 int store_all_transfers_db(FILE *fp, feed_db_t *db);
+
+/**
+ * Stores a file of trip_t records 
+ * into a given feed database instance.
+ * 
+ * @param[in]     fp    Opened file to source data from.
+ * @param[out]    db    Initialized feed database instance to populate.
+ * 
+ * @returns       Number of stored records.
+ */
 int store_all_trips_db(FILE *fp, feed_db_t *db);
 
 #endif
